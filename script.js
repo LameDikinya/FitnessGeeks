5630facc2c1a04d84ad57bb419990e8d58038d67
Lemons <cse25-245@thuto.bac.ac.bw>
2026-05-22 15:08:58 +0200

    Updated website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("feedbackForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for your feedback!");
+    function bookTrainer(name) {
+    alert("You have selected Coach " + name + " 💪");
+}
+});
