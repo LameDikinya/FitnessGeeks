d8f7369a200a641d09128e361c57e45206aaacd1
Lemons <cse25-245@thuto.bac.ac.bw>
2026-05-25 01:02:48 +0200

    Added booking page and updated trainers section

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-document.getElementById("feedbackForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you for your feedback!");
-    function bookTrainer(name) {
-    alert("You have selected Coach " + name + " 💪");
+// GET COACH NAME FROM URL
+const params = new URLSearchParams(window.location.search);
+
+const coach = params.get("coach");
+
+const coachName = document.getElementById("coachName");
+
+if (coach && coachName) {
+    coachName.innerText = "You are booking Coach " + coach + " 💪";
 }
-});
+
+
+// FEEDBACK FORM
+const feedbackForm = document.getElementById("feedbackForm");
+
+if (feedbackForm) {
+    feedbackForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        alert("Thank you for your feedback!");
+
+        feedbackForm.reset();
+    });
+}
+
+
+// BOOKING FORM
+const bookingForm = document.getElementById("bookingForm");
+
+if (bookingForm) {
+    bookingForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        alert("Booking submitted successfully!");
+
+        bookingForm.reset();
+    });
+}
